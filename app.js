25d41e2a853287bb30e2bf42792c509a377249e0
Renuka <gotocolor8@gmail.com>
2021-01-11 12:50:07 +0300

    First commit

---
+++ app.js
@@ -0,0 +1,11 @@
+console.log("hello js");
+function change() {
+	var x = Math.floor(Math.random() * 256); // range is 0-255
+	var y = Math.floor(Math.random() * 256);
+	var z = Math.floor(Math.random() * 256);
+	var thergb = "rgb(" + x + "," + y + "," + z + ")"; 
+
+	document.body.style.background=thergb;
+}
+
+// JavaScript Document
